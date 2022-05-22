d9288d04a520bd8743f6af548a5f76e6586dfd01
Peter Malmberg <peter.malmberg@gmail.com>
2022-05-22 19:31:53 +0200

    Added basic

---
+++ templates/basic.py
@@ -0,0 +1,30 @@
+# Imports -------------------------------------------------------------------
+import sys
+import os
+import traceback
+import logging
+import argparse
+from datetime import datetime, date, time
+
+# Code ----------------------------------------------------------------------
+
+def main():
+    print("Basic")
+
+
+# Main program handle 
+if __name__ == "__main__":
+    try:
+        main()
+        sys.exit(0)
+    except KeyboardInterrupt as e: # Ctrl-C
+        raise e
+    except SystemExit as e:        # sys.exit()
+        raise e
+    except Exception as e:
+        print('ERROR, UNEXPECTED EXCEPTION')
+        print(str(e))
+        traceback.print_exc()
+        os._exit(1)
+                
+
